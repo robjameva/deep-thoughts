af4b590e5440a32a4cfbdaf5d1be08e02cc0c0d0
Robert Evanik <rob.evanik@gmail.com>
2022-03-09 22:32:20 -0500

    set up Apollo server and grapghQL, also built a few queries

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas')
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const startServer = async () => {
+  // create a new Appolo server and pass in our schema
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // context: authMiddleware
+  });
+
+  // Start the server
+  await server.start();
+
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+}
+
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
